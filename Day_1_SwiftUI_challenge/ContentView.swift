d6b2550c74dad383f6b8b62c63a726a5655178ce
Fredson Silva <92945868+silvafredson@users.noreply.github.com>
2022-05-31 18:35:02 -0300

    Initial Commit

---
+++ Day_1_SwiftUI_challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day_1_SwiftUI_challenge
+//
+//  Created by Fredson Silva on 31/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day_1_SwiftUI_challenge/Day_1_SwiftUI_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_1_SwiftUI_challengeApp.swift
+//  Day_1_SwiftUI_challenge
+//
+//  Created by Fredson Silva on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Day_1_SwiftUI_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
